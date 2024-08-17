8f50af851b53b337c23e7020fe61a0c1b7cff8a0
Al-Amin <prodeveloperalamin@gmail.com>
2024-08-18 00:02:40 +0600

    Project setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAG8HNerc1p10CdLq5ThzoDE9kTeTY_5Ls",
+  authDomain: "ecommerce-e9217.firebaseapp.com",
+  projectId: "ecommerce-e9217",
+  storageBucket: "ecommerce-e9217.appspot.com",
+  messagingSenderId: "588952308882",
+  appId: "1:588952308882:web:745d7a12c9f2250738ea4b",
+}
+
+export const app = initializeApp(firebaseConfig)
